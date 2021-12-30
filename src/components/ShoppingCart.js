41e4634a52e045d6155eee4e0e1fc12f06ca83f5
Rémi Martineau <remi.martineau.dev@gmail.com>
2021-12-30 22:31:52 +0100

    created clickOutside hook & implemented it to the shopping cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,20 +1,26 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useRef} from 'react'
 import {Link} from "react-router-dom"
 
 import {useShoppingCartContext} from '../contexts/CartProvider'
+import useClickOutside from '../hooks/useClickOutside'
 import ShoppingCartItem from './ShoppingCartItem'
 
 export default function ShoppingCart({shoppingCartActive, setShoppingCartActive, setCheckoutModalActive}) {
     const {shoppingCart, setShoppingCart} = useShoppingCartContext()
     var totalItems = shoppingCart.reduce((acc, current) => acc + current.quantity, 0)
-
+    const cartRef = useRef()
     useEffect(() => {
          
     }, [shoppingCart])
 
+    useClickOutside(cartRef, () => {
+        if(shoppingCartActive)
+            setShoppingCartActive(false)
+    })
+
     return (
         <div className={`shopping-cart ${shoppingCartActive? "shopping-cart--active" : ""}`}>
-            <div className="shopping-cart__content">
+            <div ref={cartRef} className="shopping-cart__content">
                 <div className="shopping-cart__top-part">
                     <div className="shopping-cart__nb-and-remove-all">
                         <h2>{`CART (${totalItems})`}</h2>
@@ -34,7 +40,7 @@ export default function ShoppingCart({shoppingCartActive, setShoppingCartActive,
 
                     <Link
                         className="btn btn--type1"
-                        to="/checkout"
+                        to="/Audiophile/checkout"
                         onClick={() => {
                             setShoppingCartActive(false);
                             if(shoppingCart.length > 0)
